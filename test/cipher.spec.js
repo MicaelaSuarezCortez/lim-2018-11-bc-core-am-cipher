ae4610ac84671cf62088144c0980403346682c0b
MicaelaSuarezCortez <micaelasuarezco@gmail.com>
2018-12-03 10:53:48 -0500

    added negative offset and resultAnalitycs

--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -20,7 +20,14 @@ describe('cipher', () => {
 
     it('debería retornar " " para " " con offset 33',()=>{
       assert.equal(cipher.encode(33," ")," ");
-    });    
+    }); 
+
+    it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offset -33',()=>{
+      assert.equal(cipher.encode(-33,"ABCDEFGHIJKLMNOPQRSTUVWXYZ"),"HIJKLMNOPQRSTUVWXYZABCDEFG");
+    })  
+    it('debería retornar "hijklmnopqrstuvwxyzabcdefg" para "abcdefghijklmnopqrstuvwxyz" con offset -33',()=>{
+      assert.equal(cipher.encode(-33,"abcdefghijklmnopqrstuvwxyz"),"hijklmnopqrstuvwxyzabcdefg");
+    })   
   });
 
   describe('cipher.decode', () => {
@@ -40,6 +47,9 @@ describe('cipher', () => {
     it('debería retornar " " para " " con offset 33',()=>{
       assert.equal(cipher.decode(33," ")," ");
     })
+    it('debería retornar "abcdefghijklmnopqrstuvwxyz" para "hijklmnopqrstuvwxyzabcdefg" con offest 33',()=>{
+      assert.equal(cipher.decode(-33,"hijklmnopqrstuvwxyzabcdefg"),"abcdefghijklmnopqrstuvwxyz");
+    });
     
   });
 
